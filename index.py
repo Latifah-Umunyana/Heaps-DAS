d576dddab07a51fabfd5cf1c7999095cc3116836
Latifa-Umunyana <latifahumunyana@gmail.com>
2024-07-31 23:45:38 +0300

    adding heap file

--- index.py
+++ index.py
@@ -1,34 +1,38 @@
-def max_heapify(arr, j):
-    l = left(j)
-    r = right(j)
-    if l < len(arr) and arr[l] > arr[j]:
-        largest = l
-    else:
-        largest = j
-    if r < len(arr) and arr[r] > arr[largest]:
-        largest = r
-    if largest != j:
-        arr[j], arr[largest] = arr[largest], arr[j]
-        max_heapify(arr, largest)
-def left(j):
-    return 2 * j + 1
-def right(j):
-    return 2 * j + 2
-def max_heap(arr):
-    n = int((len(arr)//2)-1)
-    for j in range(n, -1, -1):
-        max_heapify(arr, j)
-def main():
-    arr = [50, 40, 50, 10, 15, 40]
-    max_heap(arr)
-    print(arr)
-if __name__=='__main__':
-     main()
-x = 'X'
-p = "X"
-def printX():
-    w = []
-    x = [2, 4, 6]
-    for i in x:
-        w.append(i * 2)
-        return w
+#Class name is Heap
+class Heap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, value):
+
+        # Adding a new value to the end of the heap using append in Python lists
+        self.heap.append(value)
+
+        #maintaining the heap property
+        self._sift_up(len(self.heap) - 1)
+
+    def _sift_up(self, index):
+        parent_index = (index - 1) // 2
+
+        # Check if the current node is greater than its parent
+        while index > 0 and self.heap[index] > self.heap[parent_index]:
+
+            # Swap the current node with its parent
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+    def get_heap(self):
+        return self.heap
+
+max_heap = Heap()
+max_heap.insert(77)
+max_heap.insert(50)
+max_heap.insert(60)
+max_heap.insert(30)
+max_heap.insert(22)
+max_heap.insert(44)
+max_heap.insert(55)
+max_heap.insert(55)
+
+print(max_heap.get_heap())
